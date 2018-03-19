1cd9eb7aaf6630a76039ac45f80fbfa79800ff61
Garcia <angelicagarcia16@augustana.edu>
2018-03-19 15:58:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/angelicagarcia16/actionbarexperiment/DinnerFragment.java
@@ -0,0 +1,20 @@
+package com.example.angelicagarcia16.actionbarexperiment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by angelicagarcia16 on 3/19/2018.
+ */
+
+public class DinnerFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Inflate layout for this fragment
+        return inflater.inflate(R.layout.fragment_dinner, container, false);
+    }
+
+}
